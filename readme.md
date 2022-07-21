617b7d66d128fc82434803dcbe8749b67ccceb4b
Michelli <michellisusanas@gmail.com>
2022-07-20 23:51:24 -0300

    HTML e CSS
    Estrutura da calculadora e estilização

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Calculadora
+
+-> Projeto criado com o intuito de praticar as tecnologias HTML, CSS e Javascript
+
+No CSS usamos o grid container
---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const equalsButton = document.querySelector('[data-operator]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
